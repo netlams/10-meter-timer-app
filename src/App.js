bec7010f138e16e3140c4242c50804498c7144ec
Dau Lam <southphillysouth@gmail.com>
2019-02-19 20:09:30 -0500

    tables added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App container-fluid">
-          <Timer />
-          <TrialTable />
+        <h1 className="display-1">10-Meter Timed Tool</h1>
+        <Timer />
+        <TrialTable />
       </div>
     );
   }
@@ -26,22 +27,23 @@ class Timer extends Component {
     let {isTiming} = this.state;
     if (!isTiming) { 
       this.start(Date.now());
-      console.log("Started");
     }
     else {
       this.stop();
-      console.log("Stopped");
+      
     }
   }
 
   start(startTime) {
+    console.log("Started");
     let {elapsed, isTiming, timerStartTime} = this.state;
-    console.log(elapsed);
-
 
     this.timer = 
       setInterval(() => {
-        elapsed = new Date() - startTime;
+        // Calculate elapsed to tenth of a second:
+        elapsed = Math.round((new Date() - startTime) / 100);
+        // Convert from seconds to One decimal place
+        elapsed = (elapsed / 10).toFixed(1);  
 
         this.setState({
           elapsed: elapsed,
@@ -51,6 +53,7 @@ class Timer extends Component {
   }
 
   stop() {
+    console.log("Stopped");
     let {elapsed, isTiming} = this.state;
 
     this.setState({
@@ -69,12 +72,7 @@ class Timer extends Component {
   }
 
   renderCountdown() {
-    // Calculate elapsed to tenth of a second:
-    var elapsed = Math.round(this.state.elapsed / 100);
-
-    // This will give a number with one digit after the decimal dot (xx.x):
-    var seconds = (elapsed / 10).toFixed(1);  
-    return <div>{seconds} seconds</div>;
+    return <div>{this.state.elapsed} seconds</div>;
   }
 
   render() {
@@ -101,6 +99,152 @@ class Timer extends Component {
   }
 }
 
+class DateMsg extends Component {
+  render() {
+    let date = new Date();
+    let todayDate = date.getMonth() + "/" + date.getDate();
+
+    return (
+      <div className="row">
+          <div id="date"><span >Date</span>: {todayDate}</div>
+        </div>
+      );
+  }
+}
+
+class TabNav extends Component {
+  render() {
+    return (
+        <ul className="nav nav-tabs" id="myTab" role="tablist">
+          <li className="nav-item">
+            <a className="nav-link active" id="ss-tab" data-toggle="tab" href="#ss" role="tab" aria-controls="ss" aria-selected="true">Self-Selected Velocity</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" id="fast-tab" data-toggle="tab" href="#fast" role="tab" aria-controls="fast" aria-selected="false">Fast Velocity</a>
+          </li>
+        </ul>
+      );
+  }
+}
+
+class TabContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trials: [{
+        id: 1,
+        value: 50.0,
+        notes: "hi"
+      }]
+    };
+  }
+
+  render() {
+    let {trials} = this.state;
+
+
+    const items = trials.map((item, key) =>
+        <tr key={item.id}>
+          <th scope="row">{item.id}</th>
+          <td>{item.value}</td>
+          <td>{item.notes}</td>
+        </tr>
+      );
+
+    return (
+      
+
+      <div className="tab-content" id="myTabContent">
+        <div className="tab-pane fade show active" id="ss" role="tabpanel" aria-labelledby="ss-tab">
+          <table className="table table-sm table-hover table-borderless">
+            <thead className="thread-dark">
+              <tr>
+                <th scope="col">Trial #</th>
+                <th scope="col">Time taken (in seconds)</th>
+                <th scope="col">Notes</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items}
+            </tbody>
+
+            
+          </table>
+          <button type="button" className="btn btn-danger" >Reset All Trials</button>
+        </div>
+
+        <div className="tab-pane fade" id="fast" role="tabpanel" aria-labelledby="fast-tab">
+          <table className="table table-sm table-hover table-borderless">
+            <thead className="thread-dark">
+              <tr>
+                <th scope="col">Trial #</th>
+                <th scope="col">Time taken (in seconds)</th>
+                <th scope="col">Notes</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>20.01</td>
+                <td>...</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>20.02</td>
+                <td>...</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>25.12</td>
+                <td>...</td>
+              </tr>
+            </tbody>
+
+            
+          </table>
+          <button type="button" className="btn btn-danger" >Reset All Trials</button>
+        </div>
+      </div>
+      );
+  }
+}
+
+class AverageTimeStat extends Component {
+  render() {
+    return (
+      <div className="row">  
+        <div className="col-sm">
+          <label>Self-Selected Average Time: 50.0</label>
+        </div>
+        <div>
+          <label>Fast Average Time: 25.0</label>
+        </div>
+        <div className="col-sm">
+          <button type="button" className="btn btn-light" >Generate Average Time (Both)</button>
+        </div>
+      </div>
+      );
+  }
+}
+
+class AverageVelocityStat extends Component {
+   render() {
+    return (
+      <div className="row">
+        <div className="col-sm">
+          <label>Self-Selected Actual Velocity 8.33 m/s</label>
+        </div>
+        <div>
+          <label>Fast Actual Velocity 5.33 m/s</label>
+        </div>
+        <div className="col-sm">
+          <button type="button" className="btn btn-light" >Generate Actual Velocity (Both)</button>  
+        </div>
+      </div>
+      );
+  }
+}
+
 class TrialTable extends Component {
   constructor(props) {
     super(props);
@@ -110,118 +254,15 @@ class TrialTable extends Component {
   }
 
   render() {
-    let todayDate = new Date();
+    
 
     return(
-      <div id="trial-table" className="container white-bg">
-        <div className="row">
-          <div id="date" className="col-sm"><span >Date</span>: {todayDate.getMonth()}/{todayDate.getDate()}</div>
-        </div>
-
-          <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item">
-              <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Self-Selected Velocity</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Fast Velocity</a>
-            </li>
-          </ul>
-          <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-              <table class="table table-striped">
-                <thead>
-                  <tr>
-                    <th scope="col">Trial #</th>
-                    <th scope="col">Time taken (in seconds)</th>
-                    <th scope="col">Notes</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>50.01</td>
-                    <td>...</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>52.02</td>
-                    <td>...</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>45.12</td>
-                    <td>...</td>
-                  </tr>
-                </tbody>
-
-                <button type="button" className="btn btn-danger" >Reset All Trials</button>
-              </table>
-
-             
-
-            </div>
-            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-              <table class="table table-striped">
-                <thead>
-                  <tr>
-                    <th scope="col">Trial #</th>
-                    <th scope="col">Time taken (in seconds)</th>
-                    <th scope="col">Notes</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>20.01</td>
-                    <td>...</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>20.02</td>
-                    <td>...</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>25.12</td>
-                    <td>...</td>
-                  </tr>
-                </tbody>
-
-                <button type="button" className="btn btn-danger" >Reset All Trials</button>
-              </table>
-            </div>
-
-             <div className="row">
-                
-                  <div className="col-sm">
-                    <label>Self-Selected Average Time: 50.0</label>
-                  </div>
-                  <div>
-                    <label>Fast Average Time: 25.0</label>
-                  </div>
-                  <div className="col-sm">
-                    <button type="button" className="btn btn-light" >Generate Average Time (Both)</button>
-                   
-                  </div>
-                
-              </div>
-
-          <div className="row">
-                  <div className="col-sm">
-                    <label>Self-Selected Actual Velocity 8.33 m/s</label>
-                  </div>
-                  <div>
-                    <label>Fast Actual Velocity 5.33 m/s</label>
-                  </div>
-
-                  <div className="col-sm">
-                    <button type="button" className="btn btn-light" >Generate Actual Velocity Time (Both)</button>
-                    
-                  </div>
-
-          </div>
-
-          </div>
+      <div id="trial-table" className="container-fluid table-bg">
+        <DateMsg />
+        <TabNav />
+        <TabContent />
+        <AverageTimeStat />
+        <AverageVelocityStat />
       </div>
       );
   }
